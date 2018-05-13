3b9d55a0e7ade17abe301c54ab9e08176ac6cf70
abhirham <savarav.a@gmail.com>
2018-05-13 18:55:02 -0400

    Added signup page

---
+++ app/src/main/java/com/example/abhirham/smackchat/createuseract.kt
@@ -0,0 +1,12 @@
+package com.example.abhirham.smackchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class createuseract : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_createuseract)
+    }
+}
--- app/src/main/java/com/example/abhirham/smackchat/loginact.kt
+++ app/src/main/java/com/example/abhirham/smackchat/loginact.kt
@@ -1,12 +1,23 @@
 package com.example.abhirham.smackchat
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_loginact.*
 
 class loginact : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginact)
+
+        signupbtn.setOnClickListener {
+            val act = Intent(this,createuseract::class.java)
+            startActivity(act)
+
+        }
+
     }
+
+
 }
